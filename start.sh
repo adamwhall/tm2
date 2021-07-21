c44f619913470a13f25013d45a814977c86f1313
AdamWHall <adamh@sphere.co.za>
2021-07-21 21:37:15 +0200

    tm2:latest

--- start.sh
+++ start.sh
@@ -8,7 +8,7 @@ then
     wget "http://files.maniaplanet.com/ManiaPlanet3Beta/ManiaPlanetBetaServer_2015-06-16.zip" -O /srv/ManiaPlanetBetaServer_2015-06-16.zip
 fi
 
-if [ ! -f /srv/ManiaPlanetBetaServer_2015-06-16.zip ]
+if [ -f /srv/ManiaPlanetBetaServer_2015-06-16.zip ]
 then  
    sudo unzip /srv/ManiaPlanetBetaServer_2015-06-16.zip -d /srv/
 fi
@@ -18,25 +18,16 @@ fi
 #     cp /srv/UserData/Config/dedicated_cfg.default.txt /srv/UserData/Config/dedicated_cfg 
 # fi
 
+cp /srv/UserData/Config/dedicated_cfg.default.txt /srv/UserData/Config/dedicated_cfg
+
 if [ ! -f /srv/RunSrvTM.sh ]
 then
     echo "./ManiaPlanetServer /title=TMStadium /game_settings=MatchSettings/TMStadiumB.txt /dedicated_cfg=dedicated_cfg.txt" > /srv/RunSrvTM.sh
-fi
-
-# Set Permissions
-if [ ! -f /srv/RunSrvTM.sh ]
-then
-    chmod +x /srv/RunSrvTM.sh
-fi
-
-if [ ! -f /srv/RunSrvTM.sh ]
-then
-    chown tm2:tm2 /srv/RunSrvTM.sh
 fi
 
 #/bin/tm2 -s /bin/bash -c '/srv/RunSrvTM.sh' tm2
 
 #usermod -aG sudo tm2
 #sudo -u tm2 /srv/RunSrvTM.sh
-cp /srv/UserData/Config/dedicated_cfg.default.txt /srv/UserData/Config/dedicated_cfg
+
 ./srv/RunSrvTM.sh
