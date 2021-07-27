71965a0ccd8e6023f2b40ea578f14038133b81a0
AdamWHall <adamh@sphere.co.za>
2021-07-27 13:11:13 +0200

    tm2:latest

--- start.sh
+++ start.sh
@@ -20,12 +20,15 @@ then
     cp /tm2srv/UserData/Config/dedicated_cfg.default.txt /tm2srv/UserData/Config/dedicated_cfg.txt
 fi
 
-if [ -f /tm2srv/RunSrvTM.sh ]
+cd /tm2srv/
+
+if [ -f RunSrvTM.sh ]
 then
     # echo "./tm2srv/ManiaPlanetServer /title=TMStadium /game_settings=MatchSettings/TMStadiumB.txt /dedicated_cfg=dedicated_cfg.txt" > /tm2srv/RunSrvTM.sh
     # echo "./tm2srv/ManiaPlanetServer /game_settings=MatchSettings/TMStadiumB.txt /dedicated_cfg=dedicated_cfg.txt /title=TMStadium" > /tm2srv/RunSrvTM.sh
-    echo "./tm2srv/ManiaPlanetServer /title=TMStadium /game_settings=MatchSettings/TMStadiumB.txt /dedicated_cfg=dedicated_cfg.txt" > /tm2srv/RunSrvTM.sh
+    echo "./ManiaPlanetServer /title=TMStadium /game_settings=MatchSettings/TMStadiumB.txt /dedicated_cfg=dedicated_cfg.txt" > /RunSrvTM.sh
 fi
 
 cd /tm2srv/
+
 ./RunSrvTM.sh
